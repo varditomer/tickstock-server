25b6b55ca6a173db32d2a4897f1b1d6fb57f4cfc
varditomer <tomer.vardi@gmail.com>
2025-05-30 11:12:31 +0300

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+});
+
+const PORT = 300;
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
